ff20f9e32077c28f7ce22be0b0d82fc82cdc5369
vuong <dinnhvuongnhoc123.vn57@gmail.com>
2021-07-08 17:05:53 +0700

    Initial Sample Data

--- trello-app/frontend/src/components/BoardContent/BoardContent.jsx
+++ trello-app/frontend/src/components/BoardContent/BoardContent.jsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './BoardContent.scss';
+import { isEmpty } from 'lodash';
 
 import Column from 'components/Column/Column';
 
+import { initialData } from 'actions/initialData';
+import { mapOrder } from 'utils/sort';
+
 function BoardContent() {
+  const [board, setBoard] = useState({});
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    const boardFromDB = initialData.boards.find(
+      (board) => board.id === 'board-1'
+    );
+    if (boardFromDB) {
+      setBoard(boardFromDB);
+
+      //sort column
+      // boardFromDB.columns.sort(function (a, b) {
+      //   return (
+      //     boardFromDB.columnOrder.indexOf(a.id) -
+      //     boardFromDB.columnOrder.indexOf(b.id)
+      //   );
+      // });
+
+      setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'));
+    }
+  }, []);
+
+  if (isEmpty(board)) {
+    return (
+      <div className='not-found' style={{ padding: '10px', color: 'white' }}>
+        Board not found
+      </div>
+    );
+  }
+
   return (
     <div className='board-content'>
-      <Column />
-      <Column />
-      <Column />
-      <Column />
-      <Column />
+      {columns.map((column, index) => (
+        <Column key={index} column={column} />
+      ))}
     </div>
   );
 }
--- trello-app/frontend/src/components/Column/Column.jsx
+++ trello-app/frontend/src/components/Column/Column.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './Column.scss';
 
-import Task from './Column';
+import { mapOrder } from 'utils/sort';
 
-function Column() {
+import Card from 'components/Card/Card.jsx';
+
+function Column(props) {
+  const { column } = props;
+  const cards = mapOrder(column.cards, column.cardOrder, 'id');
+
   return (
     <div className='column'>
-      <header>Brainstorm</header>
-      <ul className='task-list'>
-        <Task />
-        <li className='task-item'>Singleton Pattern</li>
-        <li className='task-item'>Singleton Pattern</li>
-        <li className='task-item'>Singleton Pattern</li>
-        <li className='task-item'>Singleton Pattern</li>
-        <li className='task-item'>Singleton Pattern</li>
+      <header>{column.title}</header>
+      <ul className='card-list'>
+        {cards.map((card, index) => (
+          <Card key={index} card={card} />
+        ))}
       </ul>
       <footer>Add another card</footer>
     </div>
