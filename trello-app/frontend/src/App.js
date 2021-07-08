99c52585ebf4dfb3db9d0ca216cd56b8375702a2
vuong <dinnhvuongnhoc123.vn57@gmail.com>
2021-07-08 14:53:34 +0700

    start

---
+++ trello-app/frontend/src/App.js
@@ -0,0 +1,204 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <nav className='navbar app'>App bar</nav>
+      <nav className='navbar board'>Board bar</nav>
+      <div className='board-columns'>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                className=''
+                src='https://html.sammy-codes.com/images/background.jpg'
+                alt='vuong-alt-img'
+              />
+              Title: VUONG
+            </li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                className=''
+                src='https://html.sammy-codes.com/images/background.jpg'
+                alt='vuong-alt-img'
+              />
+              Title: VUONG
+            </li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                className=''
+                src='https://html.sammy-codes.com/images/background.jpg'
+                alt='vuong-alt-img'
+              />
+              Title: VUONG
+            </li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                className=''
+                src='https://html.sammy-codes.com/images/background.jpg'
+                alt='vuong-alt-img'
+              />
+              Title: VUONG
+            </li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                className=''
+                src='https://html.sammy-codes.com/images/background.jpg'
+                alt='vuong-alt-img'
+              />
+              Title: VUONG
+            </li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                className=''
+                src='https://html.sammy-codes.com/images/background.jpg'
+                alt='vuong-alt-img'
+              />
+              Title: VUONG
+            </li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                className=''
+                src='https://html.sammy-codes.com/images/background.jpg'
+                alt='vuong-alt-img'
+              />
+              Title: VUONG
+            </li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                className=''
+                src='https://html.sammy-codes.com/images/background.jpg'
+                alt='vuong-alt-img'
+              />
+              Title: VUONG
+            </li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                className=''
+                src='https://html.sammy-codes.com/images/background.jpg'
+                alt='vuong-alt-img'
+              />
+              Title: VUONG
+            </li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                className=''
+                src='https://html.sammy-codes.com/images/background.jpg'
+                alt='vuong-alt-img'
+              />
+              Title: VUONG
+            </li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+            <li>Singleton Pattern</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
