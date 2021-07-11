1cff1883d40d04aca767536ab02982659d3a0416
vuong <dinnhvuongnhoc123.vn57@gmail.com>
2021-07-11 21:09:54 +0700

    Khởi tạo Backend

---
+++ trello-app/backend/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+const hostname = 'localhost';
+const port = 8017;
+
+app.get('/', (req, res) => {
+  res.end('<h1>hollo</h1>');
+});
+
+app.listen(port, hostname, () => {
+  console.log('running');
+});
